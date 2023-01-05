183ba85947595a94d145ed91222d39de38b80f3e
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2023-01-05 00:25:20 -0300

    corregir entidades

--- entidades.ts
+++ entidades.ts
@@ -1,72 +1,53 @@
 import { type } from "os"
+export type Usuario = {
+    id_usuario: number,
+    nombre_usuario: string,
 
- //citas
- export type Cita= {
-    id_cita:number,
-    paciente:Paciente,
-    estado_cita:String,//
-    tratamiento:Tratamiento,//
-    fecha:Date,
-    horario_inicial:number,
+}
+//citas
+export type Cita = {
+    id_cita: number,
+    fecha: Date,
+    horario_inicial: number,
     horario_final: number,
-    profesional:Profesional,
-    sucursal:Sucursal ,
-    motivo_atencion:string,
-    nombre_sillon:string,
-    lugar_atencion:string,
-    comentarios:string
+    paciente: Paciente,
+    doctor: Profesional,
+    estado_cita: Estado_cita,
+    situacion: Situacion,
 
 }
-export type Paciente={
-    id_paciente:number,
-    nombre_paciente:string,
-    nombre_social:string
+//profesionales
+export type Profesional = {
+    id_professional: number,
+    nombre: string,
+    apellido: string,
+    celular: string,
+    citas: Cita[]
 }
-export type Tratamiento={
-    id_tratamiento: number,
-    nombre_tratamiento:string
+
+export type Paciente = {
+    id_paciente: number,
+    nombre: string,
+    apellido: string
+
+}
+export type Estado_cita = {
+    id_estado_cita: number,
+    descripcion: string
+}
+export type Situacion = {
+    id_situacion: number,
+    descripcion: string
 }
 //Pagos
 export type Pagos = {
-    id_pagos:number
-    nombre_pagador:string,
-    tipo_pagador:string,
-    nombre_paciente:string,
-    monto_pago:number,
-    nombre_banco:string,
-    fecha_recepcion:Date,
-    fecha_vencimiento:Date,
-    fecha_creacion:Date,
-    rut_emisor:number    
-}
-//profesionales
-export type Profesional= {
-id_professional:number,
-usuario:Usuario,
-sucursal:Sucursal,
-rut:number,
-nombre:string,
-apellido:string,
-movil:string,
-phone:string,
-ciudad:string,
-comuna:string,
-domicilio:string,
-email:string,  // validacion mail 
-especialidad:Especialidad,
-citas:Cita []
+    id_pagos: number
+    nombre_pagador: string,
+    monto_pago: number,
+    nombre_banco: string,
+    fecha_recepcion: Date,
+    fecha_vencimiento: Date,
+    fecha_creacion: Date,
+    rut_emisor: number
 }
 
-export type Usuario={
-    id_usuario:string,
-    nombre_usuario:string
-}
-
-export type Sucursal={
-    id_sucursal:number,
-    nombre_sucursal:string
-}
-export type Especialidad={
-    id_especialidad:number,
-    nombre_especialidad:string
-}
